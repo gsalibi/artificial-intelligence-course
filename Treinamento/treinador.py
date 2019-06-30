3c5501f7ac6feeb4d814845a368b1eda1f56cc09
Gustavo <gustavohstorti@gmail.com>
2019-06-30 18:38:50 -0300

    refactoring

--- Treinamento/treinador.py
+++ Treinamento/treinador.py
@@ -1,9 +1,12 @@
-import os
-import sys
-import glob
 import dlib
 
 
+training_xml_path = "train_cleaned.xml"
+testing_xml_path = "test_cleaned.xml"
+model_name = "predictor.dat"
+
+
+# seta parâmetros
 options = dlib.shape_predictor_training_options()
 options.tree_depth = 2
 options.nu = 0.1
@@ -12,10 +15,11 @@ options.feature_pool_size = 150
 options.num_test_splits = 20
 options.be_verbose = True
 
-training_xml_path = "train_cleaned.xml"
-testing_xml_path = "test_cleaned.xml"
 
-dlib.train_shape_predictor(training_xml_path, "predictor.dat", options)
+# treina modelo
+dlib.train_shape_predictor(training_xml_path, model_name, options)
 
-print("\nTraining accuracy: {}".format( dlib.test_shape_predictor(training_xml_path, "predictor.dat")))
-print("Testing accuracy: {}".format(dlib.test_shape_predictor(testing_xml_path, "predictor.dat")))
+
+# imprime saída
+print("\nTraining accuracy: {}".format( dlib.test_shape_predictor(training_xml_path, model_name)))
+print("Testing accuracy: {}".format(dlib.test_shape_predictor(testing_xml_path, model_name)))
